bcd1a28f8982fcf9c2a218afa2dbd464336afbb2
Тишкин Андрей Алексеевич <74547328+andrei1112111@users.noreply.github.com>
2025-05-12 13:38:14 +0700

    detector

--- src/pitscndcracks/pits_cracks.py
+++ src/pitscndcracks/pits_cracks.py
@@ -1,10 +1,27 @@
+from ultralytics import YOLO
+
+
 class PitsAndCracks:
     """detection of potholes and cracks"""
 
     def __init__(self, class_numbers, model):
         """init model"""
-        pass
+        self.model = YOLO(model)
+        self.conf = 0.25
+        self.imgsz=640  # на котором обучались
 
-    def get_problem(self, frame):
+
+    def predict(self, frame):
         """get frame and return bboxes of potholes and cracks"""
-        pass
+        results = self.model.predict(frame, conf=self.conf, imgsz=self.imgsz, verbose=False)
+
+        # Получаем количество обнаруженных объектов
+        num_boxes = len(results[0].boxes)
+
+        # Наложение боксов на кадр
+        # annotated_frame = results[0].plot()
+
+        # <0 фигня
+        # = 0 нет дороги
+        # > 0 хорошая
+        return 1 if num_boxes == 0 else -1
